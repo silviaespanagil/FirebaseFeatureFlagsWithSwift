37ad037b56f8284aacea154baac1c04b8be39003
Silvia España Gil <81619759+silviaespanagil@users.noreply.github.com>
2024-09-09 14:11:51 +0200

    Create project

---
+++ FirebaseFeatureFlags/FirebaseFeatureFlags/FirebaseFeatureFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseFeatureFlagsApp.swift
+//  FirebaseFeatureFlags
+//
+//  Created by Silvia España Gil on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseFeatureFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
