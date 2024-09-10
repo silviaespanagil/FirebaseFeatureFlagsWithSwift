56ad9c0dbc265e40c16f462a24ed06b7e829c944
Silvia España Gil <81619759+silviaespanagil@users.noreply.github.com>
2024-09-10 13:30:04 +0200

    Create a simple view with background according FF value Add methods to configure and check the Feature Flag values

--- FirebaseFeatureFlags/FirebaseFeatureFlags/ContentView.swift
+++ FirebaseFeatureFlags/FirebaseFeatureFlags/ContentView.swift
@@ -6,16 +6,67 @@
 //
 
 import SwiftUI
+import FirebaseRemoteConfig
 
 struct ContentView: View {
+    
+    @State private var isBlueBackgroundEnabled = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "heart")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            (isBlueBackgroundEnabled ? Color.blue.opacity(0.5) : Color.pink.opacity(0.5))
+                .edgesIgnoringSafeArea(.all)
+            
+            HStack {
+                
+                Text("Hello, Feature Flags!")
+                    .font(.title)
+                    .foregroundColor(isBlueBackgroundEnabled ? .white : .black)
+                
+                Image(systemName: "heart")
+                    .imageScale(.large)
+                    .foregroundColor(isBlueBackgroundEnabled ? .white : .black)
+                    .onTapGesture {
+                        fetchRemoteConfig()
+                    }
+            }
         }
-        .padding()
+        .onAppear {
+            fetchRemoteConfig()
+        }
+    }
+}
+
+extension ContentView {
+    
+    
+    private func configureRemoteConfig(_ remoteConfig: RemoteConfig) {
+        
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig.configSettings = settings
+        remoteConfig.setDefaults(["isBlueBackgroundEnabled": NSNumber(value: false)])
+    }
+    
+    private func fetchRemoteConfig() {
+        
+        let remoteConfig = RemoteConfig.remoteConfig()
+        configureRemoteConfig(remoteConfig)
+        
+        remoteConfig.fetch { status, error in
+            
+            if status == .success {
+                
+                remoteConfig.activate { _, _ in
+                    self.isBlueBackgroundEnabled = remoteConfig["isBlueBackgroundEnabled"].boolValue
+                }
+            } else {
+                
+                print("Error fetching remote config: \(String(describing: error))")
+            }
+        }
     }
 }
 
